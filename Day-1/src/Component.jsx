d167fca500179b256f9fada2caed4cdcdcb0c1cb
Dikshya Maharjan <dikshyamahrzan@gmail.com>
2026-06-27 20:33:41 +0545

    feat:add img in component.jsx file

--- Day-1/src/Component.jsx
+++ Day-1/src/Component.jsx
@@ -1,6 +1,12 @@
+import hero from "./assets/hero.png";
 function Component(){
 return(
+    <>
     <button>I'm button</button>
+    <p>Hello there.<br/>How do you do?</p>
+    <img className="avatar" src={hero}/>
+    
+    </>
 )
 }
 export default Component;
